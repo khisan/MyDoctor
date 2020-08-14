f57ed84b2e0f4a6c95d99fab087954518ddd1216
khisan8@gmail.com <khisan8@gmail.com>
2020-08-15 05:24:36 +0700

    react native SVG

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View>
-      <Text>Welcome App JS</Text>
+      <Text></Text>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({});
