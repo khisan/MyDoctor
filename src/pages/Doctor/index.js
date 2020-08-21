d5679b9617fc599a3fb0478b7dc57acc597a789e
khisan <khisan8@gmail.com>
2020-08-21 20:24:18 +0700

    good news section

--- src/pages/Doctor/index.js
+++ src/pages/Doctor/index.js
@@ -14,28 +14,32 @@ const Doctor = () => {
     <View style={styles.page}>
       <View style={styles.content}>
         <ScrollView showsVerticalScrollIndicator={false}>
-          <Gap height={30} />
-          <HomeProfile />
-          <Text style={styles.welcome}>
-            Mau konsultasi dengan siapa hari ini?
-          </Text>
+          <View style={styles.wrapperSection}>
+            <Gap height={30} />
+            <HomeProfile />
+            <Text style={styles.welcome}>
+              Mau konsultasi dengan siapa hari ini?
+            </Text>
+          </View>
           <View style={styles.wrapperScroll}>
             <ScrollView horizontal showsHorizontalScrollIndicator={false}>
               <View style={styles.category}>
-                <Gap width={16} />
+                <Gap width={32} />
                 <DoctorCategory />
                 <DoctorCategory />
                 <DoctorCategory />
                 <DoctorCategory />
-                <Gap width={6} />
+                <Gap width={22} />
               </View>
             </ScrollView>
           </View>
-          <Text style={styles.sectionLabel}>Top Rated Doctors</Text>
-          <RatedDoctor />
-          <RatedDoctor />
-          <RatedDoctor />
-          <Text style={styles.sectionLabel}>Good News</Text>
+          <View style={styles.wrapperSection}>
+            <Text style={styles.sectionLabel}>Top Rated Doctors</Text>
+            <RatedDoctor />
+            <RatedDoctor />
+            <RatedDoctor />
+            <Text style={styles.sectionLabel}>Good News</Text>
+          </View>
           <NewsItem />
           <NewsItem />
           <NewsItem />
@@ -56,10 +60,10 @@ const styles = StyleSheet.create({
   content: {
     backgroundColor: colors.white,
     flex: 1,
-    paddingHorizontal: 16,
     borderBottomLeftRadius: 20,
     borderBottomRightRadius: 20,
   },
+  wrapperSection: {paddingHorizontal: 16},
   welcome: {
     fontSize: 20,
     fontFamily: fonts.primary[600],
