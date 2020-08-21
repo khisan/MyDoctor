d7a5a9994716bba93da29b71f7dd8d62e88b7dbc
khisan <khisan8@gmail.com>
2020-08-21 15:50:56 +0700

    rated doctor

--- src/components/molecules/RatedDoctor/index.js
+++ src/components/molecules/RatedDoctor/index.js
@@ -1,14 +1,50 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
+import {DummyDoctor1, IconStar} from '../../../assets';
+import {fonts} from '../../../utils/fonts';
+import {colors} from '../../../utils/colors';
 
 const RatedDoctor = () => {
   return (
-    <View>
-      <Text>Rated Doctor Component</Text>
+    <View style={styles.container}>
+      <Image source={DummyDoctor1} style={styles.avatar} />
+      <View style={styles.profile}>
+        <Text style={styles.name}>Alexa Rachel</Text>
+        <Text style={styles.category}>Pediatrician</Text>
+      </View>
+      <View style={styles.rate}>
+        <IconStar />
+        <IconStar />
+        <IconStar />
+        <IconStar />
+        <IconStar />
+      </View>
     </View>
   );
 };
 
 export default RatedDoctor;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {flexDirection: 'row', justifyContent: 'space-between'},
+  avatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 50 / 2,
+    marginRight: 12,
+    marginBottom: 16,
+  },
+  rate: {flexDirection: 'row'},
+  profile: {flex: 1},
+  name: {
+    fontSize: 16,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+  },
+  category: {
+    fontSize: 12,
+    fontFamily: fonts.primary.normal,
+    color: colors.text.secondary,
+    marginTop: 2,
+  },
+});
--- src/pages/Doctor/index.js
+++ src/pages/Doctor/index.js
@@ -13,30 +13,34 @@ const Doctor = () => {
   return (
     <View style={styles.page}>
       <View style={styles.content}>
-        <HomeProfile />
-        <Text style={styles.welcome}>
-          Mau konsultasi dengan siapa hari ini?
-        </Text>
-        <View style={styles.wrapperScroll}>
-          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-            <View style={styles.category}>
-              <Gap width={16} />
-              <DoctorCategory />
-              <DoctorCategory />
-              <DoctorCategory />
-              <DoctorCategory />
-              <Gap width={6} />
-            </View>
-          </ScrollView>
-        </View>
-        <Text style={styles.sectionLabel}>Top Rated Doctors</Text>
-        <RatedDoctor />
-        <RatedDoctor />
-        <RatedDoctor />
-        <Text style={styles.sectionLabel}>Good News</Text>
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Gap height={30} />
+          <HomeProfile />
+          <Text style={styles.welcome}>
+            Mau konsultasi dengan siapa hari ini?
+          </Text>
+          <View style={styles.wrapperScroll}>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              <View style={styles.category}>
+                <Gap width={16} />
+                <DoctorCategory />
+                <DoctorCategory />
+                <DoctorCategory />
+                <DoctorCategory />
+                <Gap width={6} />
+              </View>
+            </ScrollView>
+          </View>
+          <Text style={styles.sectionLabel}>Top Rated Doctors</Text>
+          <RatedDoctor />
+          <RatedDoctor />
+          <RatedDoctor />
+          <Text style={styles.sectionLabel}>Good News</Text>
+          <NewsItem />
+          <NewsItem />
+          <NewsItem />
+          <Gap height={30} />
+        </ScrollView>
       </View>
     </View>
   );
@@ -52,7 +56,6 @@ const styles = StyleSheet.create({
   content: {
     backgroundColor: colors.white,
     flex: 1,
-    paddingVertical: 30,
     paddingHorizontal: 16,
     borderBottomLeftRadius: 20,
     borderBottomRightRadius: 20,
