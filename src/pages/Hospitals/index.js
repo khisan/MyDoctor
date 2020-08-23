24845e7b8dc21961c050c5bd033ebe8b9305289a
khisan <khisan8@gmail.com>
2020-08-23 12:46:50 +0700

    data inline

--- src/pages/Hospitals/index.js
+++ src/pages/Hospitals/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import {StyleSheet, Text, View, ImageBackground} from 'react-native';
-import {ILHospitalBG} from '../../assets';
+import {
+  ILHospitalBG,
+  DummyHospital1,
+  DummyHospital2,
+  DummyHospital3,
+} from '../../assets';
 import {fonts, colors} from '../../utils';
 import {ListHospital} from '../../components';
 
@@ -12,9 +17,24 @@ const Hospitals = () => {
         <Text style={styles.desc}>3 Tersedia</Text>
       </ImageBackground>
       <View style={styles.content}>
-        <ListHospital />
-        <ListHospital />
-        <ListHospital />
+        <ListHospital
+          type="Rumah Sakit"
+          name="Citra Buana"
+          address="Jl.Mandraguna no.31"
+          pic={DummyHospital1}
+        />
+        <ListHospital
+          type="Rumah Sakit Anak"
+          name="Happy Family Kids"
+          address="Jl.Mandraguna no.31"
+          pic={DummyHospital2}
+        />
+        <ListHospital
+          type="Rumah Sakit Jiwa"
+          name="Tingkatan Paling Atas"
+          address="Jl.Mandraguna no.31"
+          pic={DummyHospital3}
+        />
       </View>
     </View>
   );
