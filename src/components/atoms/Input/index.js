b18266d753df69c04ad594f375b6454a5d4333c4
khisan <khisan8@gmail.com>
2020-08-27 16:08:34 +0700

    useState

--- src/components/atoms/Input/index.js
+++ src/components/atoms/Input/index.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import {StyleSheet, TextInput, View, Text} from 'react-native';
 import {colors, fonts} from '../../../utils';
 
-const Input = ({label}) => {
+const Input = ({label, value, onChangeText, secureTextEntry}) => {
   const [border, setBorder] = useState(colors.border);
   const onFocusForm = () => {
     setBorder(colors.tertiary);
@@ -17,6 +17,9 @@ const Input = ({label}) => {
         onFocus={onFocusForm}
         onBlur={onBlurForm}
         style={styles.input(border)}
+        value={value}
+        onChangeText={onChangeText}
+        secureTextEntry={secureTextEntry}
       />
     </View>
   );
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, View, ScrollView} from 'react-native';
 import {Header, Input, Button, Gap} from '../../components';
 import {colors} from '../../utils';
 
 const Register = ({navigation}) => {
+  const [fullName, setFullName] = useState('');
+  const [profession, setProfession] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onContinue = () => {
+    console.log(fullName, profession, email, password);
+    //navigation.navigate('UploadPhoto');
+  };
   return (
     <View style={styles.page}>
       <Header onPress={() => navigation.goBack()} title="Daftar Akun" />
       <View style={styles.content}>
         <ScrollView showsVerticalScrollIndicator={false}>
-          <Input label="Full Name" />
-          <Gap height={24} />
-          <Input label="Pekerjaan" />
-          <Gap height={24} />
-          <Input label="Email Address" />
-          <Gap height={24} />
-          <Input label="Password" />
-          <Gap height={40} />
-          <Button
-            title="Continue"
-            onPress={() => navigation.navigate('UploadPhoto')}
+          <Input
+            label="Full Name"
+            value={fullName}
+            onChangeText={(value) => setFullName(value)}
           />
+          <Gap height={24} />
+          <Input
+            label="Pekerjaan"
+            value={profession}
+            onChangeText={(value) => setProfession(value)}
+          />
+          <Gap height={24} />
+          <Input
+            label="Email Address"
+            value={email}
+            onChangeText={(value) => setEmail(value)}
+          />
+          <Gap height={24} />
+          <Input
+            label="Password"
+            value={password}
+            onChangeText={(value) => setPassword(value)}
+            secureTextEntry
+          />
+          <Gap height={40} />
+          <Button title="Continue" onPress={onContinue} />
         </ScrollView>
       </View>
     </View>
