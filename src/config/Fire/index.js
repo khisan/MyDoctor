dfef42fb80a108c5a71f74e7c1656fbf6100fb88
khisan <khisan8@gmail.com>
2020-08-30 04:53:07 +0700

    firebase setup

---
+++ src/config/Fire/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyABmvQ5IJKrGga-WLSCg-pZv_0pEk7WA-A',
+  authDomain: 'my-doctor-37624.firebaseapp.com',
+  databaseURL: 'https://my-doctor-37624.firebaseio.com',
+  projectId: 'my-doctor-37624',
+  storageBucket: 'my-doctor-37624.appspot.com',
+  messagingSenderId: '638556958176',
+  appId: '1:638556958176:web:e8983c9ecf2f410cc08f85',
+});
+
+const Fire = firebase;
+
+export default Fire;
